08e1963463586ba4d6eeca82a8cd017c5ddd1de1
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-11-14 15:31:18 -0600

    Added movie link with a JavaScript alert

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const input = document.getElementById('movie');
+
+function clickAlert() {
+    alert("Who, Me? Why I'm The Cat in the Hat, there's no doubt about that. I'm a super fundiferous feline, who's here to make sure that you're meline key lime turpentine. I got nothing. I'm not so good with the rhyming, not really, no. Look, I'm a cat that can talk that should be enough for you people.");
+};
+
+input.addEventListener('click', clickAlert);
